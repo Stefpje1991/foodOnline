ab8d3c2201af4903a1fc423ead5e67e55d2dd88f
Renneboog Stef <s.renneboog@llbg.com>
2022-09-03 16:46:04 +0200

    vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
+from vendor.forms import VendorForm
+
 
 # Create your views here.
 def registerUser(request):
@@ -23,3 +25,34 @@ def registerUser(request):
         'form': form
     }
     return render(request, 'accounts/registerUser.html', context)
+
+
+def registerVendor(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.set_password(password)
+            user.role = User.VENDOR
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+
+            messages.success(request, 'Your company has been registered successfully. Please wait for the approval.')
+            return redirect('registerVendor')
+        else:
+            print(form.errors)
+            print(v_form.errors)
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+    context = {
+        'form': form,
+        'v_form': v_form,
+    }
+    return render(request, 'accounts/registerVendor.html', context)
